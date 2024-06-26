c12a0952c8dd55f59b4d40956c06aea412cd353c
Manish Chaurasiya <chaurasiya.mns@gmail.com>
2024-06-26 21:28:53 +0530

    feat/infinite-scroll: infinite scroll structure setuo done

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,35 @@
 import { ChangeEvent, useCallback, useRef, useState } from "react";
 import "./App.css";
-import { InfinityScroll } from "./infinity-scroll";
+import { InfiniteScroll } from "./infinity-scroll";
 
 function App() {
-  const [querry, setQuerry] = useState("");
+  const [query, setQuery] = useState("");
   const [data, setData] = useState([]);
 
   const controllerRef: any = useRef(null);
 
   const handleInput = useCallback((event: ChangeEvent<HTMLInputElement>) => {
-    setQuerry(event.target.value);
+    setQuery(event.target.value);
   }, []);
 
-  const getData = useCallback((querry: string, pageNumber: number) => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        if (controllerRef.current) {
-          controllerRef.current.abort();
-        }
-        controllerRef.current = new AbortController();
-
-        const promise = await fetch(
-          "https://openlibrary.org/search.json?" +
-            new URLSearchParams({
-              q: querry,
-              page: `${pageNumber}`,
-            }),
-          { signal: controllerRef.current.signal }
-        );
-        const data = await promise.json();
-        resolve(data);
-        // console.log(data);
-        setData((prev) => [...prev, ...data.docs] as any);
-      } catch (error) {
-        // reject();
+  const getData = useCallback(async (querry: string, pageNumber: number) => {
+    try {
+      if (controllerRef.current) {
+        controllerRef.current.abort();
       }
-    });
+      controllerRef.current = new AbortController();
+
+      const promise = await fetch(
+        "https://openlibrary.org/search.json?" +
+          new URLSearchParams({
+            q: querry,
+            page: `${pageNumber}`,
+          }),
+        { signal: controllerRef.current.signal }
+      );
+      const data = await promise.json();
+      setData((prev) => [...prev, ...data.docs] as any);
+    } catch (error) {}
   }, []);
 
   const renderItem = useCallback(
@@ -51,12 +45,12 @@ function App() {
 
   return (
     <>
-      <input type="text" onChange={handleInput} value={querry} />
-      <InfinityScroll
+      <input type="text" onChange={handleInput} value={query} />
+      <InfiniteScroll
         renderListItem={renderItem}
         getData={getData}
         listData={data}
-        querry={querry}
+        query={query}
       />
     </>
   );
--- src/infinity-scroll.tsx
+++ src/infinity-scroll.tsx
@@ -1,13 +1,76 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useCallback, useEffect, useRef, useState } from "react";
+// /* eslint-disable react-hooks/exhaustive-deps */
+// import { useCallback, useEffect, useRef, useState } from "react";
 
-export const InfinityScroll = (props: any) => {
-  const { renderListItem, getData, listData, querry } = props;
+// export const InfinityScroll = (props: any) => {
+//   const { renderListItem, getData, listData, querry } = props;
+//   const [loading, setLoading] = useState(false);
+
+//   const pageNumber = useRef(1);
+//   const observer = useRef<IntersectionObserver | null>(null);
+//   const lastObserver = useCallback((node: any) => {
+//     console.log("loading ho rha hai", loading);
+//     if (loading) return;
+//     if (observer.current) {
+//       observer.current.disconnect();
+//     }
+
+//     observer.current = new IntersectionObserver((entries) => {
+//       if (entries[0].isIntersecting) {
+//         pageNumber.current += 1;
+//         fetchData();
+//       }
+//     });
+
+//     if (node) {
+//       observer.current.observe(node);
+//     }
+//   }, []);
+
+//   const fetchData = useCallback(async () => {
+//     if (querry) {
+//       console.log("starting");
+
+//       setLoading(true);
+//       await getData(querry, pageNumber.current).finally(() => {
+//         console.log("finally chal gaya");
+
+//         setLoading(false);
+//       });
+//     }
+//   }, [querry]);
+
+//   useEffect(() => {
+//     fetchData();
+//   }, [querry]);
+
+//   console.log("loading", loading);
+
+//   const renderList = useCallback(() => {
+//     return listData.map((item: any, index: number) => {
+//       if (index === listData.length - 1) {
+//         return renderListItem(item, index, lastObserver);
+//       }
+//       return renderListItem(item, index);
+//     });
+//   }, [listData]);
+
+//   return (
+//     <div>
+//       {renderList()}
+//       {loading && "LOADING"}
+//     </div>
+//   );
+// };
+
+import { useEffect, useRef, useState, useCallback } from "react";
+export const InfiniteScroll = (props: any) => {
+  const { renderListItem, getData, listData, query } = props;
+  const pageNumber = useRef(1);
   const [loading, setLoading] = useState(false);
 
-  const pageNumber = useRef(1);
-  const observer = useRef<IntersectionObserver | null>(null);
-  const lastObserver = useCallback((node: any) => {
+  const observer: any = useRef(null);
+
+  const lastElementOberver = (node: any) => {
     if (loading) return;
     if (observer.current) observer.current.disconnect();
 
@@ -17,40 +80,32 @@ export const InfinityScroll = (props: any) => {
         fetchData();
       }
     });
+    if (node) observer.current.observe(node);
+  };
 
-    if (node) {
-      observer.current.observe(node);
-    }
-  }, []);
+  const renderList = () => {
+    return listData.map((item: any, index: number) => {
+      if (index === listData.length - 1)
+        return renderListItem(item, index, lastElementOberver);
+      return renderListItem(item, index, null);
+    });
+  };
 
   const fetchData = useCallback(() => {
-    if (querry) {
-      setLoading(true);
-      getData(querry, pageNumber.current).finally(() => {
-        setLoading(false);
-      });
-    }
-  }, [querry]);
+    setLoading(true);
+    getData(query, pageNumber.current).finally(() => {
+      setLoading(false);
+    });
+  }, [getData, query]);
 
   useEffect(() => {
     fetchData();
   }, [fetchData]);
 
-  console.log("listdata", listData);
-
-  const renderList = useCallback(() => {
-    return listData.map((item: any, index: number) => {
-      if (index === listData.length - 1) {
-        return renderListItem(item, index, lastObserver);
-      }
-      return renderListItem(item, index);
-    });
-  }, [listData]);
-
   return (
-    <div>
+    <>
       {renderList()}
       {loading && "LOADING"}
-    </div>
+    </>
   );
 };
